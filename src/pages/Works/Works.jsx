194f397fe56738f1161f16fdc5b406fce12804a2
Augustin <augustin.berne.pro@gmail.com>
2021-08-11 14:50:07 +0200

    Nightmode is done

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -3,13 +3,16 @@ import Header from "components/Header/Header";
 
 const Works = () => {
   return (
-    <div>
+    <>
       <Header />
-      <h1>Au fil des années, nous avons pu accompagner les meilleurs.</h1>
-      <p>
-        Découvrez pas à pas comment nous avons été présent pour lancer vos marques préférées.
-      </p>
-    </div>
+      <div className="works">
+        <h1>Au fil des années, nous avons pu accompagner les meilleurs.</h1>
+        <p>
+          Découvrez pas à pas comment nous avons été présent pour lancer vos marques préférées.
+        </p>
+      </div>
+    </>
+
   );
 };
 
