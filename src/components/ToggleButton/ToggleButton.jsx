c9c46040b02fd58bfb65bdf26052dcee9a0fd873
Augustin <augustin.berne.pro@gmail.com>
2021-08-11 10:36:08 +0200

    Toggle button is started

--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 
 const ToggleButton = () => {
   return (
-    <div className="toggle-button">
-      <p>dd</p>
+    <div className="toggle-switch">
+      <input
+        type="checkbox"
+        className="toggle-switch-checkbox"
+        name="dark-light-switch"
+        id="dark-light-switch"
+      />
+      <label className="toggle-switch-label" htmlFor="dark-light-switch">
+        <span className="toggle-switch-inner" />
+        <span className="toggle-switch-switch" />
+      </label>
     </div>
   );
 };
 
 export default ToggleButton;
+
+
+// const ToggleButton = () => {
+//   return (
+//     <div className="toggle-button">
+//       <p>dd</p>
+//     </div>
+//   );
+// };
