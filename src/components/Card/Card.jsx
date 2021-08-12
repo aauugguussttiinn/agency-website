8231132b7a99b356f974bbd103944119e3fa0b4a
Augustin <augustin.berne.pro@gmail.com>
2021-08-12 18:45:43 +0200

    Editing cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Card = ({name, description, slug}) => {
   
   const shortenText = (content) => {
-    return content.length > 15 ? content.substring(0, 7) + "..." : content;
+    return content.length > 3 ? content.substring(0, 40) + "..." : content;
   }
 
   const urlToGet = (workslug) => {
@@ -14,15 +14,12 @@ const Card = ({name, description, slug}) => {
   return (
     <div class="card text-center col-3 p-0" styles="width: 18rem;">
       <div class="card-header">
-        Featured
+        { name }
       </div>
       <div class="card-body">
         <h5 class="card-title"> { name } </h5>
         <p class="card-text"> { shortenText(description) } </p>
-        <Link to={ urlToGet(slug) }> Discover </Link>
-      </div>
-      <div class="card-footer text-muted">
-        2 days ago
+        <Link to={ urlToGet(slug) }> Read more </Link>
       </div>
     </div>
   );
